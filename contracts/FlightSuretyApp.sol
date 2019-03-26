87ae28bd25de1d4c8aefb7ac01b158c3402cae5e
david2099 <loevenbruck.david@gmail.com>
2019-03-26 15:25:28 +0100

    refactor: refactor data contract

--- contracts/FlightSuretyApp.sol
+++ contracts/FlightSuretyApp.sol
@@ -1,4 +1,4 @@
-pragma solidity = 0.5.6;
+pragma solidity ^0.5.6;
 
 // It's important to avoid vulnerabilities due to numeric overflow bugs
 // OpenZeppelin's SafeMath library, when used correctly, protects agains such bugs
